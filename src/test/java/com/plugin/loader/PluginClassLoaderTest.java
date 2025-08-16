91ffe8941031f327ad231ed3a176ae3111ace2d6
yx <a357375084@gmail.com>
2025-08-16 10:50:41 +0800

    test: verify excluded packages are not overridden

--- src/test/java/com/plugin/loader/PluginClassLoaderTest.java
+++ src/test/java/com/plugin/loader/PluginClassLoaderTest.java
@@ -2,10 +2,19 @@ package com.plugin.loader;
 
 
 import org.junit.jupiter.api.Test;
+import java.net.URL;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class PluginClassLoaderTest {
     @Test
     public void TestLoaderClass() {
         System.out.println(1);
+    }
+
+    @Test
+    public void shouldNotOverrideExcludedPackages() throws ClassNotFoundException {
+        PluginClassLoader classLoader = new PluginClassLoader(new URL[0], ClassLoader.getSystemClassLoader());
+        Class<?> clazz = classLoader.loadClass("java.lang.String");
+        assertNull(clazz.getClassLoader());
     }
 }
